8ebcfe9bcc3651ca2850638d54165a0c4935f064
PiantaMK <fahc_gas@outlook.com>
2024-05-30 23:41:04 +0200

    formats

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ Example:
 """
 
 argparser = argparse.ArgumentParser(description="A tool to convert d3fc0n6's lists to valid NCC playerlists.", epilog=argparse_example)
-argparser.add_argument("-l", "--list", help="The list to convert.", choices=["bot", "cheater", "tacobot", "pazer"], required=True)
+argparser.add_argument("-l", "--list", help="The list to convert.", choices=["bot", "cheater", "tacobot", "pazer"])
+argparser.add_argument("-f", "--format", help="The output format.", choices=["ncc", "lbox", "cathook", "amalgam"])
 argparser.add_argument("-o", "--output", help="The output file.", default="output.txt")
 args = argparser.parse_args()
 
-def main(list = args.list, output = args.output):
+def main(list = args.list, fmt = args.format, output = args.output):
     url = parser.LISTS[list]
     response = requests.get(url)
     if response.status_code == 200:
@@ -23,9 +24,25 @@ def main(list = args.list, output = args.output):
             ids = parser.parse_pazer_list(response.text)
         else:
             ids = response.text.splitlines()
-        formatted = format.format_ncc_list(ids)
-        with open(output, "w") as f:
-            f.write("\n".join(formatted))
+
+        if fmt == "ncc":
+            formatted = format.format_ncc_list(ids)
+            with open(output, "w") as f:
+                f.write("\n".join(formatted))
+        elif fmt == "cathook":
+            formatted = format.format_cathook_list(ids)
+            with open(output, "w") as f:
+                f.write("\n".join(formatted))
+        elif fmt == "lbox":
+            priority = int(input("What priority do you want to assign for the bot list? (2-10): "))
+            formatted = format.format_lbox_list(ids, priority)
+            with open(output, "w") as f:
+                f.write(formatted)
+            print("Done! You have to paste in the playerlist code into your config yourself.")
+        elif fmt == "amalgam":
+            formatted = format.format_amalgam_list(ids)
+            with open(output, "w") as f:
+                f.write(formatted)
     else:
         print(f"Error: {response.status_code}")
 
--- src/format/format.py
+++ src/format/format.py
@@ -1,5 +1,37 @@
+ID64_MAGIC_NUMBER = 76561197960265728
+
 def _format_ncc(string: str) -> str:
     return f"{string} - {string}"
 
+def _format_cathook(string: str) -> str:
+    return f"cat_pl_add_id {int(string) - ID64_MAGIC_NUMBER} RAGE"
+
 def format_ncc_list(list: list) -> list:
     return [_format_ncc(i) for i in list]
+
+def format_cathook_list(list: list) -> list:
+    return [_format_cathook(i) for i in list]
+
+def format_amalgam_list(list: list) -> str:
+    formatted_list = ["{\n"]
+    for i in list:
+        formatted_list.append(f"""    "{int(i) - ID64_MAGIC_NUMBER}": [
+        "Cheater"
+    ]{"," if i != list[-1] else ""}\n""")
+    formatted_list.append("}")
+    return "".join(formatted_list)
+
+def format_lbox_list(list: list, priority: int) -> str:
+    if priority > 10:
+        priority = 10
+    if priority <= 1:
+        priority = 2
+    ret = ""
+    for i in list:
+        ret += f"{dec_to_hex(int(i) - ID64_MAGIC_NUMBER)};{priority};"
+    return ret
+
+
+def dec_to_hex(num):
+    hex_str = hex(num)[2:]
+    return hex_str
